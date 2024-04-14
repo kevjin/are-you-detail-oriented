71ecd950051e110234d2dbc3b4b29779774750b5
Kevin Jin <kevin.jin@vanderbilt.edu>
2024-04-13 21:22:29 -0400

    attempt fix scroll

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -7,7 +7,7 @@ export default function Component() {
   const location = useLocation();
   return (
     <div className="relative w-screen h-screen bg-[#F5F2DC] overflow-hidden">
-      <div className="w-full h-full pb-[5rem]">
+      <div className="w-full h-full pb-[5rem] overflow-hidden">
         <Outlet />
       </div>
       <div className="fixed bottom-0 bg-[#FFBF43] h-[5rem] w-full px-3 py-2 flex flex-row justify-between">
