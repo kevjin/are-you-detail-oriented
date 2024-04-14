700137943fa7f26ef926766a9483374e68a17f01
Kevin Jin <kevin.jin@vanderbilt.edu>
2024-04-13 20:02:17 -0400

    fix play bottom nav

--- app/routes/play.tsx
+++ app/routes/play.tsx
@@ -14,26 +14,23 @@ export default function Component() {
       <div className="h-[calc(100%-10rem)] bg-white w-full overflow-y-auto">
         <Outlet />
       </div>
-      <div className="bg-[#FFBF43] h-[5rem] w-full px-3 py-2 flex flex-row justify-center">
-        <div className="h-full flex flex-col items-center justify-center">
-          <div className="text-lg font-semibold">0:59:34</div>
-          <div className="text-sm font-medium mt-2 text-[#553608]">
-            Time Left
-          </div>
+      <div className="bg-[#FFBF43] h-[5rem] w-full px-3 py-2 flex flex-row justify-between">
+        <div className="w-[6rem] h-full flex flex-col items-center justify-center">
+          <div className="text-xl font-bold">0:59:34</div>
+          <div className="text-sm font-medium text-[#553608]">Time Left</div>
         </div>
-        <Link to="/play/results">
-          <Button className="h-full w-1/4 max-w-[5rem] rounded-md flex flex-col items-center justify-center hover:bg-[rgba(255,255,255,0.2)]">
-            <img className="w-4 h-4" src={"/icons/done.svg"} />
-            <div className="mt-2 font-medium text-sm text-[#553608]">
-              I'm done
-            </div>
-          </Button>
-        </Link>
-        <div className="h-full w-1/4 max-w-[5rem] rounded-md flex flex-col items-center justify-center hover:bg-[rgba(255,255,255,0.2)]">
-          <div className="text-lg font-semibold">{0}</div>
-          <div className="mt-2 font-medium text-sm text-[#553608]">
+        <Link
+          to="/play/results"
+          className="w-1/4 max-w-[5rem] h-full rounded-md flex flex-col items-center justify-center"
+        >
+          <img className="w-6 h-6" src={"/icons/done.svg"} />
+          <div className="mt-1 font-medium text-sm text-[#553608]">
             I'm done
           </div>
+        </Link>
+        <div className="h-full w-[6rem] rounded-md flex flex-col items-center justify-center">
+          <div className="text-xl font-bold">{0}</div>
+          <div className="font-medium text-sm text-[#553608]">Score</div>
         </div>
       </div>
     </div>
